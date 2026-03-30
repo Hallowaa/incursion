6a55e004728f0d2e569f1c269ec4caa7225277b5
Petrut-Daniel Coman <uhisu@student.kit.edu>
2026-03-30 22:31:15 +0200

    started adding action queue

--- apps/api/src/managers/IncursionManager.ts
+++ apps/api/src/managers/IncursionManager.ts
@@ -6,6 +6,7 @@ const TICK_INTERVAL = 1000 / TICK_RATE
 export default class IncursionManager {
   private incursions: Map<string, Incursion> = new Map()
   private intervalId: ReturnType<typeof setInterval> | null = null
+  private lastTime = Date.now()
 
   public addIncursion(id: string, incursion: Incursion) {
     this.incursions.set(id, incursion)
@@ -28,7 +29,11 @@ export default class IncursionManager {
   }
 
   private start() {
-    this.intervalId = setInterval(() => this.tick(), TICK_INTERVAL)
+    this.intervalId = setInterval(() => {
+      const delta = Date.now() - this.lastTime
+
+      this.tick(delta)
+    }, TICK_INTERVAL)
   }
 
   private stop() {
@@ -38,7 +43,7 @@ export default class IncursionManager {
     }
   }
 
-  private tick() {
+  private tick(delta: number) {
     // TODO: implement tick logic for all active incursions
   }
 }
--- apps/api/src/models/domain/incursion/Incursion.ts
+++ apps/api/src/models/domain/incursion/Incursion.ts
@@ -1,7 +1,16 @@
 import type { IncursionId, IncursionTheme } from '@incursion/dto'
+import type Ability from '../ability/Ability'
+import type Character from '../entity/Character'
+import type Entity from '../entity/Entity'
 import type IncursionRoom from './IncursionRoom'
+import { EntityKind } from '@incursion/dto'
 
 export default class Incursion {
+  private queuedActions: {
+    user: Entity
+    action: Ability
+  }[] = []
+
   public constructor(
     public incursionId: IncursionId,
     public name: string,
@@ -10,4 +19,29 @@ export default class Incursion {
     public currentRoom: IncursionRoom,
     public theme: IncursionTheme
   ) {}
+
+  public tick(delta: number) {
+
+  }
+
+  public queueAction(user: Entity, action: Ability) {
+    if (action.canUse(user, this)) {
+      this.queuedActions.push({ user, action })
+    } else {
+      console.warn(`${user.entityId} tried to use ${action.abilityId} but can't use it.`)
+    }
+  }
+
+  public processQueuedActions() {
+    for (const qa of this.queuedActions) {
+      qa.action.execute(qa.user, this)
+    }
+  }
+
+  public processCooldowns(delta: number) {
+    for (const iie of this.currentRoom.entities) {
+      // reduce cooldown timer
+      // TODO: add AbilityInstance to iie
+    }
+  }
 }
